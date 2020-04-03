c372c9ef842426a060b6558aae4dd2672175a41d
unknown <devinenoise@gmail.com>
2020-04-03 14:46:15 -0700

    add github repos to profile page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 import { getProfileById } from '../../actions/profile'
 
 const Profile = ({
@@ -62,9 +63,12 @@ const Profile = ({
                 ))}
               </Fragment>
             ) : (
-              <h4>No education yet</h4>
+              <h4>No education credentials</h4>
             )}
           </div>
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername} />
+          )}
         </Fragment>
       )}
     </Fragment>
