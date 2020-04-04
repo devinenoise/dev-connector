8c30eb149494a0556a9c6800e7d2ca96e4d38cb4
unknown <devinenoise@gmail.com>
2020-04-04 01:00:51 -0700

    deploy heroku

---
+++ client/build/precache-manifest.e185400a081ebde232b280ea3b8c32f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd29a0a36fcb3a3ed4f932decd899c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640a57476cf225fff045",
+    "url": "/static/css/main.83648ea1.chunk.css"
+  },
+  {
+    "revision": "b971e0ca9b60f38d473e",
+    "url": "/static/js/2.e7543bb8.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.e7543bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640a57476cf225fff045",
+    "url": "/static/js/main.e8abb326.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
