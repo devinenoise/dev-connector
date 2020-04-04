193984fa61a9ee483034a48f5019fc8a0727772a
unknown <devinenoise@gmail.com>
2020-04-04 10:38:37 -0700

    auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,27 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
-module.exports = function(req, res, next) {
-  // Get token from the header
-  const token = req.header('x-auth-token');
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token')
 
-  // If token not found
+  // Check if not token
   if (!token) {
-    return res.status(401).json({ message: 'No token, authorization denied' });
+    return res.status(401).json({ msg: 'No token, authorization denied' })
   }
 
   // Verify token
-
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-    req.user = decoded.user;
-    next();
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        res.status(401).json({ msg: 'Token is not valid' })
+      } else {
+        req.user = decoded.user
+        next()
+      }
+    })
   } catch (err) {
-    res.status(401).json({ message: 'Token is not valid' });
+    console.error('something wrong with auth middleware')
+    res.status(500).json({ msg: 'Server Error' })
   }
-};
+}
